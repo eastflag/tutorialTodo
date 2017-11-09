e7918051c4f0b807b5e3062867db515c92df2b1c
eastflag <eastflag@gmail.com>
2017-11-10 00:47:47 +0900

    angular 메뉴 - 애니메이션 적용

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,12 +1,25 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {TodoVo} from "../domain/todo.vo";
 import {UserService} from "../user.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-angular',
   templateUrl: './angular.component.html',
   styleUrls: ['./angular.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({transform: 'translate(0, 0)'})),
+      transition('void => *', [
+        style({transform: 'translate(-100%, 0)'}),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({transform: 'translate(0, -100%)', opacity: '0'}))
+      ])
+    ])
+  ]
 })
 export class AngularComponent implements OnInit {
 
