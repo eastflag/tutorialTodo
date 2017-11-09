4523d9ec20b7a82289c3f9337b4dea88cf95f78c
eastflag <eastflag@gmail.com>
2017-11-10 01:34:41 +0900

    .

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -64,21 +64,14 @@ export class AngularComponent implements OnInit {
       });
   }
 
-  remove(todoVo: TodoVo) {
-    const result = confirm(todoVo.todo + '을(를) 삭제하시겠습니까?');
-    if (result) {
-      this.userService.removeTodo(todoVo.todo_id)
-        .then(res => {
-          if (res.result === 0) {
-            this.todoList.forEach((item, index) => {
-              if (item.todo_id === todoVo.todo_id) {
-                this.todoList.splice(index, 1);
-                }
-            });
-          }
-        });
-    }
+  restore(todoVo: TodoVo) {
+    todoVo.isEdited = false;
+
+    let tempTodo = this.tempTodoList.get(todoVo.todo_id);
+    todoVo.isFinished = tempTodo.isFinished;
+    todoVo.todo = tempTodo.todo;
   }
+
 
   save(todoVo: TodoVo) {
     todoVo.isEdited = true;
@@ -89,11 +82,19 @@ export class AngularComponent implements OnInit {
     this.tempTodoList.set(todoVo.todo_id, tempTodo);
   }
 
-  restore(todoVo: TodoVo) {
-    todoVo.isEdited = false;
-
-    let tempTodo = this.tempTodoList.get(todoVo.todo_id);
-    todoVo.isFinished = tempTodo.isFinished;
-    todoVo.todo = tempTodo.todo;
+  remove(todoVo: TodoVo) {
+    const result = confirm(todoVo.todo + '을(를) 삭제하시겠습니까?');
+    if (result) {
+      this.userService.removeTodo(todoVo.todo_id)
+        .then(res => {
+          if (res.result === 0) {
+            this.todoList.forEach((item, index) => {
+              if (item.todo_id === todoVo.todo_id) {
+                this.todoList.splice(index, 1);
+              }
+            });
+          }
+        });
+    }
   }
 }
