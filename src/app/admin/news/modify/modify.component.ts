ebc913418ae7bc236ce55a6dcc68040da5415ee1
eastflag <eastflag@gmail.com>
2017-11-13 02:27:25 +0900

    뉴스 - 수정 구현, 중복된 부분은 child component로 해야함.

--- src/app/admin/news/modify/modify.component.ts
+++ src/app/admin/news/modify/modify.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {NewsVO} from "../../../domain/news.vo";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AdminService} from "../../admin.service";
+import {MatSnackBar, MatSnackBarConfig} from "@angular/material";
 
 @Component({
   selector: 'app-modify',
@@ -7,10 +11,67 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ModifyComponent implements OnInit {
+  news_id: number;
+  news: NewsVO = new NewsVO();
 
-  constructor() { }
+  fileList: FileList;
+
+  constructor(private route: ActivatedRoute, private adminService: AdminService, private router: Router,
+              private snackBar: MatSnackBar) {
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      let news_id = +params['news_id'];
+      this.adminService.findOneNews(news_id)
+        .then((res: NewsVO) => {
+          this.news = res;
+        });
+    });
+  }
+
+  modifyNews() {
+    this.adminService.modifyNews(this.news)
+      .then(res => {
+        if (res['result'] === 0) {
+          this.router.navigate(['../..'], {relativeTo: this.route});
+          let config = new MatSnackBarConfig();
+          config.duration = 3000;
+          this.snackBar.open('수정하였습니다.', null, config);
+        }
+      });
+  }
+
+  fileChange(event: any) {
+    this.fileList = event.target.files;
+
+    let reader = new FileReader();
+    reader.readAsDataURL((this.fileList[0]));
+    reader.onload = () => {
+      this.imageUpload();
+    };
+  }
+
+  imageUpload() {
+    let formData: FormData = new FormData();
+
+    if (this.fileList && this.fileList.length > 0) {
+      let file: File = this.fileList[0];
+      formData.append('file', file, file.name);
+    }
+
+    this.adminService.imageUpload(formData)
+      .then(res => {
+        if (res['result'] === 0) {
+          // 이미지 경로를  editor에 추가한다.
+          console.log(res['value']);
+          if (this.news.content) {
+            this.news.content += `<img src="http://www.javabrain.kr${res['value']}" style="max-width: 100%;">`;
+          } else {
+            this.news.content = `<img src="http://www.javabrain.kr${res['value']}" style="max-width: 100%;">`;
+          }
+        }
+      });
   }
 
 }
