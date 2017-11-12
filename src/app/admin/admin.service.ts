d1efbbd8e2270a4671c22bd28fde2017eb5ff4c0
eastflag <eastflag@gmail.com>
2017-11-12 20:32:21 +0900

    typescript 오버로딩, 디폴트, 옵셔널 연산자

---
+++ src/app/admin/admin.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {RequestOptions} from "@angular/http";
+
+@Injectable()
+export class AdminService {
+
+  private SERVER: string;
+  private headers: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.SERVER = `${environment.HOST}`;
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+  }
+
+  // 뉴스 관리 ---------------------------------------------------------------------------------------------------------
+  findNews(params: any): Promise<any>  {
+    return this.http.post(this.SERVER + '/api/newsList', JSON.stringify(params), {headers: this.headers})
+      .toPromise();
+  }
+
+  findOneNews(params: any) {
+    return this.http.get(this.SERVER + '/api/news?news_id=' + params)
+      .toPromise();
+  }
+
+  addNews(params: any) {
+    return this.http.post(this.SERVER + '/api/news', JSON.stringify(params), {headers: this.headers})
+      .toPromise();
+  }
+
+  modifyNews(params: any) {
+    return this.http.put(this.SERVER + '/api/news', JSON.stringify(params), {headers: this.headers})
+      .toPromise();
+  }
+
+  removeNews(params: any) {
+    return this.http.delete(this.SERVER + '/api/news?news_id=' + params)
+      .toPromise();
+  }
+
+  imageUpload(formData: FormData) {
+    let headers = new HttpHeaders();
+    // headers.append('Content-Type', 'multipart/form-data'); //브라우저가 자동 생성함.
+    // headers.append("Authorization", "Bearer " + sessionStorage.getItem("admin_token"));
+    headers.append('Accept', 'application/json');
+
+    return this.http.post(this.SERVER + '/api/imageUpload', formData, {headers: headers})
+      .toPromise();
+  }
+}
--- src/app/admin/news/news.component.ts
+++ src/app/admin/news/news.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {PageVO} from "../../domain/page.vo";
+import {AdminService} from "../admin.service";
+import {NewsVO} from "../../domain/news.vo";
 
 @Component({
   selector: 'app-news',
@@ -7,10 +10,33 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class NewsComponent implements OnInit {
+  page: PageVO = new PageVO(0, 5);
+  newsList: Array<NewsVO>;
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
   ngOnInit() {
+    this.getNewsList();
   }
 
+
+  getNewsList() {
+    let params = {
+      start_index: this.page.pageIndex * this.page.pageSize,
+      page_size: this.page.pageSize
+    };
+    this.adminService.findNews(params)
+      .then(res => {
+        console.log(res);
+        this.newsList = res['data'];
+        this.page.totalCount = res['total'];
+      });
+  }
+
+  pageChanged(event: any) {
+    console.log(event);
+    this.page.pageIndex = event.pageIndex;
+    this.page.pageSize = event.pageSize;
+    this.getNewsList();
+  }
 }
