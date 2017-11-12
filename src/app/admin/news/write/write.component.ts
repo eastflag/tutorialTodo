0eff37ea64fda54f08eb63fc318a8b05dca46ac4
eastflag <eastflag@gmail.com>
2017-11-13 01:50:33 +0900

    뉴스 - 글쓰기, 로컬 파일 업로드

--- src/app/admin/news/write/write.component.ts
+++ src/app/admin/news/write/write.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {NewsVO} from "../../../domain/news.vo";
 import {MatSnackBar, MatSnackBarConfig} from "@angular/material";
 import {AdminService} from "../../admin.service";
@@ -12,6 +12,11 @@ import {Router} from "@angular/router";
 })
 export class WriteComponent implements OnInit {
   news = new NewsVO();
+
+  fileList: FileList;
+
+  // @ViewChild('newFile')
+  // newFile: any;
 
   constructor(private snackBar: MatSnackBar, private adminService: AdminService, private router: Router) { }
 
@@ -29,6 +34,40 @@ export class WriteComponent implements OnInit {
       .then(res => {
         if (res['result'] === 0) {
           this.router.navigateByUrl('/admin/news');
+        }
+      });
+  }
+
+  fileChange(event: any) {
+    this.fileList = event.target.files;
+    console.log(this.fileList);
+    // show thumbnail
+    let reader = new FileReader();
+    reader.readAsDataURL((this.fileList[0]));
+    reader.onload = () => {
+      // this.thumbnailSrc = reader.result;
+      this.imageUpload();
+    };
+  }
+
+  imageUpload() {
+    let formData: FormData = new FormData();
+
+    if (this.fileList && this.fileList.length > 0) {
+      let file: File = this.fileList[0];
+      formData.append('file', file, file.name);
+    }
+
+    this.adminService.imageUpload(formData)
+      .then(res => {
+        if (res['result'] === 0) {
+          // 이미지 경로를  editor에 추가한다.
+          console.log(res['value']);
+          if (this.news.content) {
+            this.news.content += `<img src="http://www.javabrain.kr${res['value']} style="max-width: 100%;"">`;
+          } else {
+            this.news.content = `<img src="http://www.javabrain.kr${res['value']}" style="max-width: 100%;">`;
+          }
         }
       });
   }
