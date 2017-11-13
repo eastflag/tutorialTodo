1f2d9f976ff484b608cc4746c3055c58c528d2ed
eastflag <eastflag@gmail.com>
2017-11-13 11:40:54 +0900

    삭제시 다이얼로그 처리

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,17 +5,19 @@ import { NewsComponent } from './news/news.component';
 import { HomeComponent } from './home/home.component';
 import {AdminRoutingModule} from "./admin-routing";
 import {
-  MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatPaginatorModule, MatSnackBar,
+  MatButtonModule, MatCardModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule,
+  MatPaginatorModule, MatSnackBar,
   MatSnackBarModule,
   MatToolbarModule
 } from "@angular/material";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {AdminService} from "./admin.service";
 import { WriteComponent } from './news/write/write.component';
-import { ViewComponent } from './news/view/view.component';
+import {ViewComponent} from './news/view/view.component';
 import {FormsModule} from "@angular/forms";
 import {CKEditorModule} from "ng2-ckeditor";
 import { ModifyComponent } from './news/modify/modify.component';
+import {ViewDialogComponent} from "./news/view/view.dialog.component";
 
 @NgModule({
   imports: [
@@ -32,8 +34,10 @@ import { ModifyComponent } from './news/modify/modify.component';
     MatSnackBarModule,
     MatFormFieldModule,
     MatInputModule,
+    MatDialogModule,
   ],
-  declarations: [AdminComponent, NewsComponent, HomeComponent, WriteComponent, ViewComponent, ModifyComponent],
-  providers: [AdminService]
+  declarations: [AdminComponent, NewsComponent, HomeComponent, WriteComponent, ViewComponent, ModifyComponent, ViewDialogComponent],
+  providers: [AdminService],
+  entryComponents: [ViewDialogComponent]
 })
 export class AdminModule { }
--- src/app/admin/news/view/view.component.ts
+++ src/app/admin/news/view/view.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {AdminService} from "../../admin.service";
 import {NewsVO} from "../../../domain/news.vo";
 import {PlatformLocation} from "@angular/common";
-import {MatSnackBar, MatSnackBarConfig} from "@angular/material";
+import {MatDialog, MatSnackBar, MatSnackBarConfig} from "@angular/material";
+import {ViewDialogComponent} from "./view.dialog.component";
 
 @Component({
   selector: 'app-view',
@@ -15,7 +16,7 @@ export class ViewComponent implements OnInit {
   news: NewsVO;
 
   constructor(private route: ActivatedRoute, private adminService: AdminService, private location: PlatformLocation,
-              private router: Router, private snackBar: MatSnackBar) { }
+              private router: Router, private snackBar: MatSnackBar, private dialog: MatDialog) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -33,17 +34,35 @@ export class ViewComponent implements OnInit {
   }
 
   confirmDelete(news: NewsVO) {
-    const result = confirm('삭제하시겠습니까?');
-    if (result) {
-      this.adminService.removeNews(news.news_id)
-        .then(value => {
-          if (value['result'] === 0) {
-            let conf = new MatSnackBarConfig();
-            conf.duration = 3000;
-            this.snackBar.open('삭제하였습니다.', null, conf);
-            this.router.navigate(['../..'], {relativeTo: this.route});
-          }
-        });
-    }
+/*    const result = confirm('삭제하시겠습니까?');
+     if (result) {
+     this.adminService.removeNews(news.news_id)
+     .then(value => {
+     if (value['result'] === 0) {
+     let conf = new MatSnackBarConfig();
+     conf.duration = 3000;
+     this.snackBar.open('삭제하였습니다.', null, conf);
+     this.router.navigate(['../..'], {relativeTo: this.route});
+     }
+     });
+     }*/
+
+    let dialogRef = this.dialog.open(ViewDialogComponent, {
+      data: {content: '삭제하시겠습니까?'}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) { // 삭제하기
+        this.adminService.removeNews(news.news_id)
+          .then(value => {
+            if (value['result'] === 0) {
+              let conf = new MatSnackBarConfig();
+              conf.duration = 3000;
+              this.snackBar.open('삭제하였습니다.', null, conf);
+              this.router.navigate(['../..'], {relativeTo: this.route});
+            }
+          });
+      }
+    });
   }
 }
