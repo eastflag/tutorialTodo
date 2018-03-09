be7aca11ea56740733007615eada53026e0c88bc
eastflag <eastflag@gmail.com>
2018-03-09 19:14:05 +0900

    이미지 슬라이더

--- js/es5/16_closure.js
+++ js/es5/16_closure.js
@@ -1,12 +1,7 @@
-
-function myFunc() {
-
-  for(var i=0; i < 3; ++i) {
-    return function() {
-      console.log(i);
-    }
+function init() {
+  var name = "Mozilla"; //클로저, 외부에서 접근 불가능한 변수
+  return function displayName() {
+    console.log(name); // 부모 함수에서 선언된 변수를 사용한다
   }
 }
-
-
-myFunc()();
+init()();
