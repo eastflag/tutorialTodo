90abd36fabb2c81a661a902084ab8b8f4e4d16a4
eastflag <eastflag@gmail.com>
2018-03-23 15:16:32 +0900

    mat-icon svg 아이콘으로 변경. 애니메이션 추가

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {AuthGuardService} from "../auth/auth-guard.service";
+import {MatIconRegistry} from "@angular/material";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-index',
@@ -9,9 +11,38 @@ import {AuthGuardService} from "../auth/auth-guard.service";
 })
 export class IndexComponent implements OnInit {
 
-  constructor(public authService: AuthGuardService) { }
+  constructor(public authService: AuthGuardService, private matIconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      "jquery",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/ic_extension_black_24px.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "angular",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/ic_change_history_black_24px.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "news",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/ic_dashboard_black_24px.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "person",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/ic_person_black_24px.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "login",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/ic_input_black_24px.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "logout",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/ic_power_settings_new_black_24px.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "nickname",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/ic_build_black_24px.svg")
+    );
+  }
 
   ngOnInit() {
   }
-
 }
