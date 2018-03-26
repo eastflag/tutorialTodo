aaedc8a9167c6a3ea69fc10833b0ae0df5443334
eastflag <eastflag@gmail.com>
2018-03-26 20:09:06 +0900

    chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from "../user.service";
+import {AuthGuardService} from "../auth/auth-guard.service";
+import {MemberVO} from "../domain/member.vo";
+import {ChatVO} from "../domain/chat.vo";
 
 @Component({
   selector: 'app-chat',
@@ -6,20 +10,38 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit, OnDestroy {
-
+  member: MemberVO;
+  message: string;
   ws: WebSocket;
 
-  constructor() { }
+  constructor(private userService: UserService, private authService: AuthGuardService) {
+
+  }
 
   ngOnInit() {
+    this.userService.getMember(this.authService.getMemberId())
+      .subscribe(body => {
+        this.member = body;
+        this.init();
+      });
+  }
+
+  init() {
     this.ws = new WebSocket("ws://localhost:8080/api/chat");
 
     this.ws.onopen = () => {
-      this.ws.send("send");
+
     };
 
     this.ws.onmessage = (ev) => {
       console.log(ev.data);
+      const chat: ChatVO = ev.data as ChatVO;
+
+      if ("WhoAreYou" === chat.command) {
+        this.ws.send(JSON.stringify({command: "IAmTom"}));
+      } else if ("Receive" === chat.command) {
+        console.log(chat.message);
+      }
     };
 
     this.ws.onclose = () => {
@@ -31,4 +53,8 @@ export class ChatComponent implements OnInit, OnDestroy {
     this.ws.close();
   }
 
+
+  send() {
+    this.ws.send(JSON.stringify({command: "Send", message: this.message}));
+  }
 }
---
+++ src/app/domain/chat.vo.ts
@@ -0,0 +1,7 @@
+export class ChatVO {
+  session_id?: string;
+  member_id?: number;
+  nickname?: string; // nickname
+  command?: string; // 프로토콜
+  message?: string;
+}
